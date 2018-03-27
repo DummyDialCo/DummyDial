1893c2abbf1d842de61b62163e3b1d96a7d799bc
rajanrai93 <rajanrai93@icloud.com>
2018-03-27 13:49:36 -0700

    added text input function

--- components/TextPage.js
+++ components/TextPage.js
@@ -13,19 +13,32 @@ export default class TextPage extends React.Component {
 	}
 
   sendText = () => {
-    fetch("https://quiet-fortress-33478.herokuapp.com/"+this.state.recipient);
+    fetch("https://quiet-fortress-33478.herokuapp.com/"+this.state.recipient+"/"+this.state.myMsg);
   }
 
   render() {
     return (
       <View>
+        
+        
 				<TextInput
 					placeholder="Enter your number"
 					onChangeText={(recipient)=>this.setState({recipient})}
 					value={this.state.recipient}
 				/>
+                
+                <TextInput 
+                  placeholder="Enter your message"
+                  ref={(el)=>{this.myMsg=el;}}
+                  onChangeText={(myMsg)=>this.setState({myMsg})}
+                  value={this.state.myMsg}
+                  placeholderTextColor="black"
+                />
+                
 
         <Button onPress={this.sendText} title="SEND TEXT" color="orange" />
+            
+            
       </View>
     );
   }
