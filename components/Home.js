77d16f4ce4aceeceaa260a427b00b95998266ce5
Kim Wilson <kimwilson@kims-air.wifi.bcit.ca>
2018-03-28 14:06:01 -0700

    stuff

--- components/Home.js
+++ components/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from "react-navigation";
 
+import Welcome from "./Welcome";
+
 import CallBtn from "./CallBtn";
-
 import CallPage from "./CallPage";
-import TextPage from "./TextPage";
-import AddContact from "./AddContact";
 
+import TextPage from "./TextPage";
+
+import AddContact from "./AddContact";
 import Timer from "./Timer";
 
 import styles from "./styles/Home";
@@ -23,11 +25,9 @@ export default class Home extends React.Component {
   render() {
     return (
       <View>
-				<Text>DUMMY DIAL</Text>
-				<Text>DUMMY DIAL</Text>
-				<Text>DUMMY DIAL</Text>
-
-
+		<Text>DUMMY DIAL</Text>
+		<Text>DUMMY DIAL</Text>
+		<Text>DUMMY DIAL</Text>
         <Welcome />
       </View>
     );
--- components/Welcome.js
+++ components/Welcome.js
@@ -5,21 +5,16 @@ import { StackNavigator } from "react-navigation";
 export default class Welcome extends React.Component {
 
 	constructor(props){
+		
 		super(props);
-
 		this.state = {
 
 		}
 	}
 
-
-
   render() {
     return (
       <View>
-
-
-
       </View>
     );
   }
