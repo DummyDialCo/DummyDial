539d862dd5569947e28a2051ba65284df34bbcf0
gregsakai <gregsakai@gmail.com>
2018-03-28 13:51:06 -0700

    Made Welcome page with AsyncStorage

--- components/Home.js
+++ components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from "react-navigation";
 
-import CallBtn from "./CallBtn";
+import Welcome from "./Welcome";
 
 import CallPage from "./CallPage";
 import TextPage from "./TextPage";
--- components/Welcome.js
+++ components/Welcome.js
@@ -8,17 +8,32 @@ export default class Welcome extends React.Component {
 		super(props);
 
 		this.state = {
-
+      recipient:"",
+      completedSignUp:false
 		}
 	}
 
+  finishSignup = () => {
 
+    // Stores the number asynchronously
+    AsyncStorage.setItem("storeTheNum", this.state.recipient).catch((err)=>{
+      console.log(err);
+    });
+
+    this.setState({
+      completedSignUp:true
+    });
+  }
 
   render() {
     return (
       <View>
 
+        <Text>Welcome to Dummy Dial</Text>
 
+        <TextInput placeholder="Enter your phone number"  />
+
+        <Button onPress={this.finishSignup} title="CONTINUE" />
 
       </View>
     );
