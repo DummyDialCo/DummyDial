6346f11bdef9d25b1eff3384fdcafac1f9954e90
gregsakai <gregsakai@gmail.com>
2018-03-29 09:43:18 -0700

    StackNavigator in App.js only

--- App.js
+++ App.js
@@ -3,31 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from "react-navigation";
 
 import Welcome from "./components/Welcome";
-
+import Home from "./components/Home";
+import Menu from "./components/Menu";
 import CallPage from "./components/CallPage";
 import TextPage from "./components/TextPage";
 import AddContact from "./components/AddContact";
 
-import Timer from "./components/Timer";
-
 /* Only Stack Navigator controls in this file */
 const DummyDial = StackNavigator({
+  Home: { screen: Home },
   Welcome: { screen: Welcome },
+  Menu: { screen: Menu },
   CallPage: { screen: CallPage },
   TextPage: { screen: TextPage },
   AddContact: { screen: AddContact }
 }, {
-  initialRouteName: 'Welcome',
-  headerMode: 'screen'
+  initialRouteName: "Welcome",
+  headerMode: "Screen"
 });
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <View>
-		    <DummyDial />
-      </View>
+      <DummyDial />
     );
+
   }
 }
