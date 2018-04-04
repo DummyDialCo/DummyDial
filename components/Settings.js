6c0c0fee9ca698bae1ab390ea84ba1d0caad5501
gregsakai <gregsakai@gmail.com>
2018-04-04 11:07:57 -0700

    Settings page allows new phone number

--- components/Settings.js
+++ components/Settings.js
@@ -18,16 +18,29 @@ export default class CallPage extends React.Component {
 
 	componentDidMount = () => {
 
-		// AsyncStorage.getItem("storeTheNum").then((value)=>{
-    //   if (value !== null){
-    //     console.log("Stored number -", value);
-    //     this.setState({
-    //       recipient:value
-    //     })
-    //   }
-    // }).catch((err)=>{
-    //   console.log(err);
-    // });
+		AsyncStorage.getItem("storeTheNum").then((value)=>{
+      if (value !== null){
+        console.log("Stored number -", value);
+        this.setState({
+          recipient:value
+        })
+      }
+    }).catch((err)=>{
+      console.log(err);
+    });
+	}
+
+
+	changePhoneNumber = () => {
+		// Stores the phone number in AsyncStorage
+    AsyncStorage.setItem("storeTheNum", this.state.recipient).catch((err)=>{
+      console.log(err);
+    });
+
+    // TODO Add RegEx to validate phone number
+    if(this.refs.newPhoneInput === "")
+      console.log("PHONE NUMBER EMPTY");
+
 	}
 
 
@@ -37,13 +50,46 @@ export default class CallPage extends React.Component {
 
     return (
       <View>
-		<Text></Text>
-		<Text></Text>
-		<Text></Text>
-		<Text></Text>
-		<Text></Text>
+		<Text>
+			{"\n"}
+			{"\n"}
+			{"\n"}
+			{"\n"}
+			THIS IS THE SETTINGS PAGE!
+			{"\n"}
+			{"\n"}
+			{"\n"}
+			{"\n"}
+		</Text>
 
 			<Text>YOUR PHONE NUMBER: {this.state.recipient}</Text>
+
+			<Text>
+				{"\n"}
+				{"\n"}
+				{"\n"}
+				{"\n"}
+			</Text>
+
+			<Text>CHANGE YOUR PHONE NUMBER HERE</Text>
+			<TextInput
+				placeholder="New phone number..."
+				ref="newPhoneInput"
+				onChangeText={(recipient)=>this.setState({recipient})}
+				value={this.state.recipient}
+			/>
+
+			<TouchableOpacity onPress={this.changePhoneNumber}>
+				<Text>SAVE</Text>
+			</TouchableOpacity>
+
+
+			<Text>
+				{"\n"}
+				{"\n"}
+				{"\n"}
+				{"\n"}
+			</Text>
 
 			<TouchableOpacity onPress={()=>{navigate("Home")}}>
 				<Text>CLICK TO RETURN TO HOME</Text>
