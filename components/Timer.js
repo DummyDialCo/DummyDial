88e3008062f31726189f4744dd4581302667a2b6
gregsakai <gregsakai@gmail.com>
2018-04-23 19:50:54 -0700

    Button transition back and forth works

--- components/Timer.js
+++ components/Timer.js
@@ -22,7 +22,24 @@ export default class Timer extends React.Component {
     this.state = {
       recipient: this.props.navigation.state.params.recipient,
       myMsg: "",
+      // time props
+      minsRemaining: 0,
+      secsRemaining: 0,
+      // progress bar
+      progress: 0,
+      progressBarColor: "transparent",
+      // btn styling - !clicked turns grey
+      callBtnStyles: Styles.btn,
+      textBtnStyles: Styles.btn,
+
+      // nav bar hides when timer starts
+      navBarHiding: Styles.navBar
     }
+  }
+
+
+  getInitialState() {
+    return { progress: -1 };
   }
 
   componentDidMount = () => {
@@ -42,16 +59,50 @@ export default class Timer extends React.Component {
     .catch(error => {
       console.log(error);
     });
-
   };
+
+
+  startedTimer = () => {
+    this.setState({
+      navBarHiding: Styles.navBarHidden
+    });
+  }
 
 
   render() {
     const { navigate } = this.props.navigation;
 
-		var innerDisplay = (
-			<Text>Timer</Text>
-		);
+    var innerDisplay = (
+        <View
+          style={{
+            width: 200,
+            height: 200,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#f6f6f6"
+          }}
+        >
+          <View style={Styles.timerInputContainer}>
+            <TextInput
+              style={Styles.timerInp}
+              placeholder="00"
+              returnKeyType="done"
+              keyboardType="number-pad"
+              onChangeText={minsRemaining => this.setState({ minsRemaining })}
+            />
+            <Text style={Styles.timerInp}>:</Text>
+            <TextInput
+              style={Styles.timerInp}
+              placeholder="00"
+              returnKeyType="done"
+              keyboardType="number-pad"
+              onChangeText={secsRemaining => this.setState({ secsRemaining })}
+            />
+          </View>
+        </View>
+      );
+
 
     return (
       <View style={Styles.all}>
@@ -91,35 +142,27 @@ export default class Timer extends React.Component {
         </Text>
 
         <View style={Styles.BtnCont}>
-          <TouchableOpacity
-            style={this.state.callButton}
-            onPress={() => {
-              this.setState({
-								callButton: Styles.btn,
-                textButton: Styles.greyBtn
-              });
-							if(this.state.totalTimeRemaining <= 0){
-								this.startTimer();
-							} else if (this.state.prevInterval != null){
-								clearInterval(this.state.prevInterval);
-							}
-
-            }}
-          >
-            <Image
-              source={require("./imgs/phonew.png")}
-              style={{ width: 19, height: 21 }}
-            />
+          <TouchableOpacity style={this.state.callBtnStyles} onPress={()=>{
+            this.startedTimer();
+            this.setState({
+              callBtnStyles: Styles.btn,
+              textBtnStyles: Styles.greyBtn
+            });
+          }}>
+            <Image source={require("./imgs/phonew.png")} style={{ width: 19, height: 21 }} />
             <Text style={Styles.btnTTxt}>Call</Text>
           </TouchableOpacity>
 
           <View style={Styles.smBreak3} />
 
-          <TouchableOpacity>
-            <Image
-              source={require("./imgs/textw.png")}
-              style={{ width: 27, height: 21 }}
-            />
+          <TouchableOpacity style={this.state.textBtnStyles} onPress={()=>{
+            this.startedTimer();
+            this.setState({
+              callBtnStyles: Styles.greyBtn,
+              textBtnStyles: Styles.btn
+            });
+          }}>
+            <Image source={require("./imgs/textw.png")} style={{ width: 27, height: 21 }} />
             <Text style={Styles.btnTTxt}>Text</Text>
           </TouchableOpacity>
         </View>
@@ -132,7 +175,16 @@ export default class Timer extends React.Component {
 				<Text>{"\n"}</Text>
 
 
-        <View style={this.state.navStyling}>
+				<TouchableOpacity onPress={this.resetTimer}>
+					<Text>{this.state.exitTimerMessage}</Text>
+				</TouchableOpacity>
+
+
+
+
+
+
+        <View style={this.state.navBarHiding}>
           <TouchableOpacity onPress={() => navigate("Home", { recipient: this.state.recipient })}>
             <View style={Styles.navBarBtn}>
               <Image
