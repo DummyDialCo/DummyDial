28017e831bb5d069b263888dc01512265ad3b7de
gregsakai <gregsakai@gmail.com>
2018-04-25 13:43:47 -0700

    Fixed timer second bug, input editing while paused, and reset function

--- components/Timer.js
+++ components/Timer.js
@@ -43,8 +43,8 @@ export default class Timer extends React.Component {
       navBarHiding: Styles.navBar, // style changes to hidden when timer is running
       pauseBtn: "", // button text = change to icon later
       stopBtn: "", // button text - change to icon later
-      displayingInputs:true,
-      isPaused: false,
+      displayingInputs: true,
+      isPaused: true,
       mode: null, // controls whether pause/play is active
       timeString: "...", // displayed briefly after timer is initialized
       displayBlack: Styles.blckBGhidden,
@@ -68,26 +68,29 @@ export default class Timer extends React.Component {
   };
 
   startedTimer = () => {
-    var totalSeconds = parseFloat(this.state.minsRemaining * 60) + parseFloat(this.state.secsRemaining);
+    var totalSeconds =
+      parseFloat(this.state.minsRemaining * 60) +
+      parseFloat(this.state.secsRemaining);
     this.setState({
       initialTotalSeconds: totalSeconds,
       navBarHiding: Styles.navBarHidden,
       exitTimerMessage: "Click here to exit timer",
       pauseBtn: "PAUSE",
       stopBtn: "STOP",
+      isPaused: false,
+      totalSeconds: totalSeconds
     });
     console.log(this.state.initialTotalSeconds);
 
-    var beginCount = setInterval(() => {
+    this.beginCount = setInterval(() => {
       // checks if the timer is paused, or if there is already
       // an instance running if a timer is already running,
       // clicking between call/text will not create a new timer
       if (!this.state.isPaused) {
-
         var minZero = "";
         var secZero = "";
-        var minutesDigit = Math.floor(totalSeconds / 60);
-        var secondsDigit = totalSeconds % 60;
+        var minutesDigit = Math.floor(this.state.totalSeconds / 60);
+        var secondsDigit = this.state.totalSeconds % 60;
         if (minutesDigit <= 9) minZero = "0";
         if (secondsDigit <= 9) secZero = "0";
 
@@ -95,30 +98,34 @@ export default class Timer extends React.Component {
           timeString: minZero + minutesDigit + ":" + secZero + secondsDigit,
           progressBarColor: "#5CACEE",
           newMinsRemaining: minZero + minutesDigit,
-          newSecsRemaining: secZero + secondsDigit
+          newSecsRemaining: secZero + secondsDigit,
+          minsRemaining:
+            minZero + Math.floor(this.state.totalSeconds / 60) + "",
+          secsRemaining: secZero + this.state.totalSeconds % 60 + ""
         });
 
         if (this.state.totalSeconds > -1) {
-          totalSeconds--;
           this.setState({
-            totalSeconds: totalSeconds,
+            totalSeconds: this.state.totalSeconds - 1,
             progress: this.state.progress + 1
           });
           console.log("Time remaining:", this.state.totalSeconds);
 
           if (this.state.totalSeconds === -1) {
-            clearInterval(beginCount);
+            if (this.state.clickedCallBtn) {
+              fetch(
+                "https://dummydial93.herokuapp.com/" + this.state.recipient
+              );
+            } else if (this.state.clickedTextBtn) {
+              fetch(
+                "https://quiet-fortress-33478.herokuapp.com/" +
+                  this.state.recipient +
+                  "/" +
+                  this.state.myMsg
+              );
+            }
 
-            // if(this.state.clickedCallBtn)
-            //   fetch("https://dummydial93.herokuapp.com/"+this.state.recipient);
-            // else if(this.state.clickedTextBtn)
-            //   fetch("https://quiet-fortress-33478.herokuapp.com/"+this.state.recipient+"/"+this.state.myMsg);
-
-            this.setState({
-              displayingInputs: true,
-              timerIsRunning: false,
-            });
-						// this.resetTimer();
+            this.resetTimer();
           }
         }
       }
@@ -132,7 +139,9 @@ export default class Timer extends React.Component {
         isPaused: true,
         mode: true,
         pauseBtn: "PLAY",
-        displayingInputs: true
+        displayingInputs: true,
+        minsRemaining: Math.floor(this.state.totalSeconds / 60) + "",
+        secsRemaining: this.state.totalSeconds % 60 + ""
       });
       return;
     } else if (this.state.mode) {
@@ -141,92 +150,134 @@ export default class Timer extends React.Component {
         isPaused: false,
         mode: false,
         pauseBtn: "PAUSE",
-        displayingInputs: false
+        displayingInputs: false,
+        totalSeconds:
+          parseFloat(this.state.minsRemaining * 60) +
+          parseFloat(this.state.secsRemaining)
       });
+      console.log(
+        "calculation",
+        parseFloat(this.state.minsRemaining * 60),
+        parseFloat(this.state.secsRemaining)
+      );
       return;
     }
   };
 
   resetTimer = () => {
-    console.log("Clicked reset");
-    // reset function
+    clearInterval(this.beginCount);
+    this.setState({
+      minsRemaining: "00",
+      secsRemaining: "00",
+      totalSeconds: 0,
+      initialTotalSeconds: 0,
+      newMinsRemaining: "00",
+      newSecsRemaining: "00",
+      progress: -1,
+      progressBarColor: "transparent",
+      callBtnStyles: Styles.btn,
+      textBtnStyles: Styles.btn,
+      clickedCallBtn: false,
+      clickedTextBtn: false,
+      timerIsRunning: false,
+      navBarHiding: Styles.navBar,
+      pauseBtn: "",
+      stopBtn: "",
+      displayingInputs: true,
+      isPaused: true,
+      mode: null,
+      timeString: "...",
+      displayBlack: Styles.blckBGhidden,
+      blckActive: null
+    });
   };
 
-    
-    displayedBlack = () => {
-       if(!this.state.blckActive){
-           this.setState({
-               displayBlack: Styles.blckBG,
-               blckActive: true
-           })
-       } else {
-           this.setState({
-               displayBlack: Styles.blckBGhidden,
-               blckActive: false
-           })
-       }
-    };
-    
-    
+  displayedBlack = () => {
+    if (!this.state.blckActive) {
+      this.setState({
+        displayBlack: Styles.blckBG,
+        blckActive: true
+      });
+    } else {
+      this.setState({
+        displayBlack: Styles.blckBGhidden,
+        blckActive: false
+      });
+    }
+  };
+
   render() {
     const { navigate } = this.props.navigation;
 
-    var innerDisplay;
-
-    if (!this.state.displayingInputs) {
-      innerDisplay = (
-        <View style={Styles.circleInnerDisplay}>
-          <Text style={Styles.timerInp}>{this.state.timeString}</Text>
-
-          <TouchableOpacity onPress={this.pauseTimer}>
-            <Text>{this.state.pauseBtn}</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={this.resetTimer}>
-            <Text>{this.state.stopBtn}</Text>
-          </TouchableOpacity>
+    var innerDisplay = (
+      <View style={Styles.circleInnerDisplay}>
+        <View style={Styles.timerInputContainer}>
+          <TextInput
+            style={Styles.timerInpMins}
+            placeholder={this.state.newMinsRemaining}
+            returnKeyType="done"
+            keyboardType="number-pad"
+            editable={this.state.isPaused}
+            onChangeText={minsRemaining => {
+              if (this.state.isPaused) {
+                console.log(minsRemaining);
+                minsRemaining = parseInt(minsRemaining);
+                if (minsRemaining < 10) {
+                  minsRemaining = "0" + minsRemaining;
+                }
+                console.log(minsRemaining);
+                minsRemaining = "" + minsRemaining;
+                minsRemaining =
+                  minsRemaining[minsRemaining.length - 2] +
+                  minsRemaining[minsRemaining.length - 1];
+                this.setState({ minsRemaining });
+              }
+            }}
+            value={this.state.minsRemaining}
+            // NOTE IF MINSREMAINING DECREMENTED DOWN, IT COULD BE USED FOR SETTING THE TIMER WHEN PAUSED
+          />
+          <Text style={Styles.timerInp}>:</Text>
+          <TextInput
+            style={Styles.timerInp}
+            placeholder={this.state.newSecsRemaining}
+            returnKeyType="done"
+            keyboardType="number-pad"
+            editable={this.state.isPaused}
+            onChangeText={secsRemaining => {
+              if (this.state.isPaused) {
+                console.log(secsRemaining);
+                secsRemaining = parseInt(secsRemaining);
+                if (secsRemaining < 10) {
+                  secsRemaining = "0" + secsRemaining;
+                }
+                console.log(secsRemaining);
+                secsRemaining = "" + secsRemaining;
+                secsRemaining =
+                  secsRemaining[secsRemaining.length - 2] +
+                  secsRemaining[secsRemaining.length - 1];
+                this.setState({ secsRemaining });
+              }
+            }}
+            value={this.state.secsRemaining}
+          />
         </View>
-      );
-    } else {
-      innerDisplay = (
-        <View style={Styles.circleInnerDisplay}>
-          <View style={Styles.timerInputContainer}>
-            <TextInput
-              style={Styles.timerInpMins}
-              placeholder={this.state.newMinsRemaining}
-              returnKeyType="done"
-              keyboardType="number-pad"
-              onChangeText={minsRemaining => this.setState({ minsRemaining })}
+        <TouchableOpacity onPress={this.pauseTimer}>
+          <Text>{this.state.pauseBtn}</Text>
+        </TouchableOpacity>
 
-              // NOTE IF MINSREMAINING DECREMENTED DOWN, IT COULD BE USED FOR SETTING THE TIMER WHEN PAUSED
+        <TouchableOpacity onPress={this.resetTimer}>
+          <Text>{this.state.stopBtn}</Text>
+        </TouchableOpacity>
+      </View>
+    );
 
-            />
-            <Text style={Styles.timerInp}>:</Text>
-            <TextInput
-              style={Styles.timerInp}
-              placeholder={this.state.newSecsRemaining}
-              returnKeyType="done"
-              keyboardType="number-pad"
-              onChangeText={secsRemaining => this.setState({ secsRemaining })}
-            />
-          </View>
-          <TouchableOpacity onPress={this.pauseTimer}>
-            <Text>{this.state.pauseBtn}</Text>
-          </TouchableOpacity>
+    var blckScreen = (
+      <TouchableOpacity
+        onPress={this.displayedBlack}
+        style={this.state.displayBlack}
+      />
+    );
 
-          <TouchableOpacity onPress={this.resetTimer}>
-            <Text>{this.state.stopBtn}</Text>
-          </TouchableOpacity>
-        </View>
-      );
-    }
-
-var blckScreen = (
-    <TouchableOpacity onPress={this.displayedBlack} style={this.state.displayBlack}>
-    </TouchableOpacity>
-
-);
-                  
     return (
       <View style={Styles.all}>
         <View style={Styles.tBan}>
@@ -244,15 +295,11 @@ var blckScreen = (
           {"\n"}
         </Text>
 
-        <TouchableOpacity onPress={this.displayedBlack}> 
-            <Text style={Styles.blueTxt}> 
-            Tap here for black screen
-            </Text>
+        <TouchableOpacity onPress={this.displayedBlack}>
+          <Text style={Styles.blueTxt}>Tap here for black screen</Text>
         </TouchableOpacity>
-        
-        <Text>     
-          {"\n"}
-        </Text>
+
+        <Text>{"\n"}</Text>
 
         <CircularProgressDisplay.Hollow
           size={200}
@@ -261,7 +308,7 @@ var blckScreen = (
           progressBarColor={this.state.progressBarColor}
           easing="linear"
           innerComponent={innerDisplay}
-          rotate={this.state.progress / this.state.initialTotalSeconds * 360}
+          rotate={this.state.progress / this.state.totalSeconds * 360}
         />
 
         <Text>
@@ -273,7 +320,7 @@ var blckScreen = (
           <TouchableOpacity
             style={this.state.callBtnStyles}
             onPress={() => {
-              if(!this.state.timerIsRunning){
+              if (!this.state.timerIsRunning) {
                 this.startedTimer();
               }
               this.setState({
@@ -298,7 +345,7 @@ var blckScreen = (
           <TouchableOpacity
             style={this.state.textBtnStyles}
             onPress={() => {
-              if(!this.state.timerIsRunning){
+              if (!this.state.timerIsRunning) {
                 this.startedTimer();
               }
               this.setState({
@@ -320,7 +367,16 @@ var blckScreen = (
         </View>
 
         <Text>
-          {"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
+          {"\n"}
         </Text>
 
         <TouchableOpacity onPress={this.resetTimer}>
@@ -384,7 +440,7 @@ var blckScreen = (
             </View>
           </TouchableOpacity>
         </View>
-                {blckScreen}
+        {blckScreen}
       </View>
     );
   }
