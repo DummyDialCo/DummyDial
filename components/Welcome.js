b677a72e590261753f5f1b05d87b8561537c0830
Kim Wilson <kimwilson@kims-air.wifi.bcit.ca>
2018-04-27 13:24:03 -0700

    style

--- components/Welcome.js
+++ components/Welcome.js
@@ -50,30 +50,39 @@ export default class Welcome extends React.Component {
     return (
       <View style={Styles.all}>
         <View style={Styles.tBan}>
-          <Text>{"\n"}</Text>
+        <Text>
+		{"\n"}
+		</Text>
           <Text style={Styles.tBanTitle}>Welcome</Text>
         </View>
 
         <Text>
-          {"\n"}
-          {"\n"}
+        {"\n"}
+		{"\n"}
         </Text>
 
         <KeyboardAvoidingView>
           <View style={Styles.all}>
             <Image
               source={require("./imgs/DDLogo.png")}
-              style={{ width: 111, height: 140 }}
+              style={{ width: 88, height: 110 }}
             />
 
             <View style={Styles.smBreak2} />
 
             <Text style={Styles.title}>DummyDial</Text>
-            <Text> </Text>
-            <View style={Styles.smBreak} />
+			
+            <Text>
+				{"\n"}
+				{"\n"}
+			</Text>
+			
+			<Text style={Styles.steps2}>
+              Calls & texts will be sent to this number
+            </Text>
 
-            <View style={Styles.smBreak2} />
-
+			<View style={Styles.smBreak2} />
+			  
             <View style={Styles.inptIcnCont}>
               <TextInput
                 style={this.state.inputFormStatus}
@@ -90,30 +99,40 @@ export default class Welcome extends React.Component {
                 }}
               />
             </View>
+			  
+			  <View style={Styles.smBreak} />
+			  
+			 <Text style={Styles.steps2}>
+              Calls & texts will appear with this ID
+            </Text>
+
+			<View style={Styles.smBreak2} />
+			  
+            <View style={Styles.inptIcnCont}>
+              <TextInput
+                style={this.state.inputFormStatus}
+                placeholder="Enter fake contact name"
+              />
+            </View>
 
             <View style={Styles.smBreak2} />
 
             <Text style={Styles.star}>
-              * This can be changed later in settings
+              * These can be changed later in settings
             </Text>
 
-            <Text>
+           	<Text>
               {"\n"}
-              <Text> </Text>
-              <Text> </Text>
             </Text>
+			
             <TouchableOpacity style={Styles.btn} onPress={this.finishSignup}>
               <Text style={Styles.btnTxt}>Submit</Text>
             </TouchableOpacity>
 
-            <Text>
-              {"\n"}
-              {"\n"}
-              {"\n"}
-            </Text>
+            <View style={Styles.smBreak} />
 
             <TouchableOpacity style={Styles.qInfo}>
-              <Text style={Styles.blueTxt}>Why do you need my number</Text>
+              <Text style={Styles.blueTxt}>FAQ</Text>
               <Text> </Text>
               <View style={Styles.qMrk}>
                 <Text style={Styles.qMrkTxt}>?</Text>
