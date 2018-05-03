b69c9b3d9020cb2f0728bddf64824d9e5d3098b5
gregsakai <gregsakai@gmail.com>
2018-05-03 13:43:43 -0700

    Added Slider

--- components/Timer.js
+++ components/Timer.js
@@ -3,7 +3,6 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
   Button,
   TouchableOpacity,
   Image,
@@ -14,7 +13,7 @@ import {
 import { StackNavigator } from "react-navigation";
 import Styles from "./scss/Styles.scss";
 
-import CircularProgressDisplay from "react-native-progress-circular";
+import Slider from "react-native-slider";
 
 export default class Timer extends React.Component {
   constructor(props) {
@@ -24,37 +23,20 @@ export default class Timer extends React.Component {
       recipient: this.props.navigation.state.params.recipient,
       myMsg: "",
       // time props
-      minsRemaining: "00",
-      secsRemaining: "00",
+      value: 0,
       totalSeconds: 0,
-      initialTotalSeconds: 0,
-      newMinsRemaining: "00", // becomes the input placeholders when paused
-      newSecsRemaining: "00", // becomes the input placeholders when paused
-      // progress bar
-      progress: -1,
-      progressBarColor: "transparent", // turns blue when timer starts
+      timeString: "00:00",
       // btn styling - !clicked turns grey
       callBtnStyles: Styles.btn,
       textBtnStyles: Styles.btn,
       // evaluates which button was clicked
       clickedCallBtn: false,
       clickedTextBtn: false,
-      // events after the timer starts
-      timerIsRunning: false,
+      // events after the timer starts\
       navBarHiding: Styles.navBar, // style changes to hidden when timer is running
-      pauseBtn: null, // source of the image - changes once timer starts
-      stopBtn: null, // source of the image - changes once timer starts
-      displayingInputs: true,
-      isPaused: true,
-      mode: null, // controls whether pause/play is active
-      timeString: "...", // displayed briefly after timer is initialized
       displayBlack: Styles.blckBGhidden, // styling changes to display: flex; after
       blckActive: null // function changes this to a boolean
     };
-  }
-
-  getInitialState() {
-    return { progress: -1 };
   }
 
   componentDidMount = () => {
@@ -83,52 +65,38 @@ export default class Timer extends React.Component {
 		Keyboard.dismiss();
   	}
 
+
   startedTimer = () => {
 	Keyboard.dismiss();
 
-    var totalSeconds =
-      parseFloat(this.state.minsRemaining * 60) +
-      parseFloat(this.state.secsRemaining);
+
+
     this.setState({
-      initialTotalSeconds: totalSeconds,
       navBarHiding: Styles.navBarHidden,
-      exitTimerMessage: "Tap here to exit timer",
-      pauseBtn: require("./imgs/pauseicon.png"),
-      stopBtn: require("./imgs/stopicon.png"),
-      isPaused: false,
-      totalSeconds: totalSeconds
+      exitTimerMessage: "Tap here to exit timer"
     });
 
     this.beginCount = setInterval(() => {
-      // checks if the timer is paused, or if there is already
-      // an instance running if a timer is already running,
-      // clicking between call/text will not create a new timer
-      if (!this.state.isPaused) {
-        var minZero = "";
-        var secZero = "";
-        var minutesDigit = Math.floor(this.state.totalSeconds / 60);
-        var secondsDigit = this.state.totalSeconds % 60;
-        if (minutesDigit <= 9) minZero = "0";
-        if (secondsDigit <= 9) secZero = "0";
 
-        this.setState({
-          timeString: minZero + minutesDigit + ":" + secZero + secondsDigit,
-          progressBarColor: "#5CACEE",
-          newMinsRemaining: minZero + minutesDigit,
-          newSecsRemaining: secZero + secondsDigit,
-          minsRemaining:
-            minZero + Math.floor(this.state.totalSeconds / 60) + "", // + "" converts it to a string
-          secsRemaining: secZero + this.state.totalSeconds % 60 + ""
-        });
 
-        if (this.state.totalSeconds > -1) {
+      var minZero = "";
+      var minDigit = Math.floor(this.state.totalSeconds / 60);
+      var secZero = "";
+      var secDigit = this.state.totalSeconds % 60;
+      if (minDigit <= 9) minZero = "0";
+      if(secDigit <= 9 ) secZero = "0";
+      this.setState({
+        timeString: minZero + minDigit + ":" + secZero + secDigit
+      });
+
+        if (this.state.totalSeconds > 0) {
           this.setState({
             totalSeconds: this.state.totalSeconds - 1,
-            progress: this.state.progress + 1
+            // value: (this.state.value/60) - 1
           });
           console.log("Time remaining:", this.state.totalSeconds);
 
-          if (this.state.totalSeconds === -1) {
+          if (this.state.totalSeconds === 0) {
 
             if (this.state.clickedCallBtn) {
               fetch("https://dummydial93.herokuapp.com/"+this.state.recipient);
@@ -138,68 +106,14 @@ export default class Timer extends React.Component {
 
             this.resetTimer();
           }
-        }
       }
     }, 1000);
-  };
-
-  pauseTimer = () => {
-    if (!this.state.mode) {
-      // PAUSED
-      var minZero = "";
-      if(Math.floor(this.state.totalSeconds / 60) < 10) minZero = "0";
-      var secZero = "";
-      if((this.state.totalSeconds % 60) < 10) secZero = "0";
-      this.setState({
-        isPaused: true,
-        mode: true,
-        pauseBtn: require("./imgs/playicon.png"),
-        displayingInputs: true,
-        minsRemaining: minZero + Math.floor(this.state.totalSeconds / 60) + "",
-        secsRemaining: secZero + this.state.totalSeconds % 60 + ""
-      });
-      return;
-    } else if (this.state.mode) {
-      // UNPAUSED
-      var totalSeconds = parseFloat(this.state.minsRemaining * 60) + parseFloat(this.state.secsRemaining);
-      this.setState({
-        progress: 0,
-        isPaused: false,
-        mode: false,
-        pauseBtn: require("./imgs/pauseicon.png"),
-        displayingInputs: false,
-        totalSeconds: totalSeconds,
-        initialTotalSeconds: totalSeconds
-      });
-      return;
-    }
   };
 
   resetTimer = () => {
     clearInterval(this.beginCount);
     this.setState({
-      minsRemaining: "00",
-      secsRemaining: "00",
-      totalSeconds: 0,
-      initialTotalSeconds: 0,
-      newMinsRemaining: "00",
-      newSecsRemaining: "00",
-      progress: -1,
-      progressBarColor: "transparent",
-      callBtnStyles: Styles.btn,
-      textBtnStyles: Styles.btn,
-      clickedCallBtn: false,
-      clickedTextBtn: false,
-      timerIsRunning: false,
-      navBarHiding: Styles.navBar,
-      pauseBtn: null,
-      stopBtn: null,
-      displayingInputs: true,
-      isPaused: true,
-      mode: null,
-      timeString: "...",
-      displayBlack: Styles.blckBGhidden,
-      blckActive: null
+
     });
   };
 
@@ -219,80 +133,6 @@ export default class Timer extends React.Component {
 
   render() {
     const { navigate } = this.props.navigation;
-
-    var innerDisplay = (
-      <View style={Styles.circleInnerDisplay}>
-
-		<Text>
-        {"\n"}
-		{"\n"}
-		{"\n"}
-		{"\n"}
-       </Text>
-
-        <View style={Styles.timerInputContainer}>
-          <TextInput
-            style={Styles.timerInpMins}
-            placeholder={this.state.newMinsRemaining}
-            returnKeyType="done"
-            keyboardType="number-pad"
-            editable={this.state.isPaused}
-            onChangeText={minsRemaining => {
-              if (this.state.isPaused) {
-                minsRemaining = parseInt(minsRemaining);
-                if (minsRemaining < 10) {
-                  minsRemaining = "0" + minsRemaining;
-                }
-                minsRemaining = "" + minsRemaining;
-                minsRemaining =
-                  minsRemaining[minsRemaining.length - 2] +
-                  minsRemaining[minsRemaining.length - 1];
-                this.setState({ minsRemaining });
-              }
-            }}
-            value={this.state.minsRemaining}
-            // NOTE IF MINSREMAINING DECREMENTED DOWN, IT COULD BE USED FOR SETTING THE TIMER WHEN PAUSED
-          />
-          <Text style={Styles.timerInp}>:</Text>
-          <TextInput
-            style={Styles.timerInp}
-            placeholder={this.state.newSecsRemaining}
-            returnKeyType="done"
-            keyboardType="number-pad"
-            editable={this.state.isPaused}
-            onChangeText={secsRemaining => {
-              if (this.state.isPaused) {
-                secsRemaining = parseInt(secsRemaining);
-                if (secsRemaining < 10) {
-                  secsRemaining = "0" + secsRemaining;
-                }
-                secsRemaining = "" + secsRemaining;
-                secsRemaining =
-                  secsRemaining[secsRemaining.length - 2] +
-                  secsRemaining[secsRemaining.length - 1];
-                this.setState({ secsRemaining });
-              }
-            }}
-            value={this.state.secsRemaining}
-          />
-        </View>
-
-		<View style={Styles.smBreak2}/>
-
-        <View style={Styles.pausePlayBtnCont}>
-          <TouchableOpacity onPress={this.pauseTimer}>
-            <Image source={this.state.pauseBtn} style={{ width: 30, height: 30 }} />
-          </TouchableOpacity>
-
-		<View style={Styles.smBreak4}/>
-
-          <TouchableOpacity onPress={this.resetTimer}>
-            <Image source={this.state.stopBtn} style={{ width: 30, height: 30 }} />
-          </TouchableOpacity>
-        </View>
-
-      </View>
-    );
 
     var blckScreen = (
       <TouchableOpacity
@@ -326,15 +166,43 @@ export default class Timer extends React.Component {
 
 		<View style={Styles.smBreak} />
 
-        <CircularProgressDisplay.Hollow
-          size={250}
-          progressBarWidth={9}
-          backgroundColor={"#ffffff"}
-          progressBarColor={this.state.progressBarColor}
-          easing="linear"
-          innerComponent={innerDisplay}
-          rotate={this.state.progress / this.state.initialTotalSeconds * 360}
-        />
+    <Slider
+            trackStyle={{ height: 20, backgroundColor: "#fff", borderRadius: 50 }}
+            thumbStyle={{ width: 40, height: 40, borderRadius: 20 }}
+            thumbTintColor={"#5cacee"}
+            animateTransitions={true}
+            minimumTrackTintColor={"#5cacee"}
+
+
+              style={{
+                width: 300,
+    direction: "ltr",
+    borderColor: "red"
+              }}
+              value={this.state.value}
+              step={1}
+              minimumValue={0}
+              maximumValue={30}
+              onValueChange={(value) => {
+                this.setState({
+                  value: value,
+                  totalSeconds: value * 60
+
+                });
+
+                var minZero = "";
+                var minDigit = Math.floor(this.state.totalSeconds / 60);
+                var secZero = "";
+                var secDigit = this.state.totalSeconds % 60;
+                if (minDigit <= 9) minZero = "0";
+                if(secDigit <= 9 ) secZero = "0";
+                this.setState({
+                  timeString: minZero + minDigit + ":" + secZero + secDigit
+                });
+
+              }} />
+
+            <Text>{this.state.timeString}</Text>
 
         <Text>{"\n"}</Text>
 
@@ -349,9 +217,7 @@ export default class Timer extends React.Component {
                 callBtnStyles: Styles.btn,
                 textBtnStyles: Styles.greyBtn,
                 clickedCallBtn: true,
-                clickedTextBtn: false,
-                displayingInputs: false,
-                timerIsRunning: true
+                clickedTextBtn: false
               });
             }}
           >
@@ -374,9 +240,7 @@ export default class Timer extends React.Component {
                 callBtnStyles: Styles.greyBtn,
                 textBtnStyles: Styles.btn,
                 clickedCallBtn: false,
-                clickedTextBtn: true,
-                displayingInputs: false,
-                timerIsRunning: true
+                clickedTextBtn: true
               });
             }}
           >
