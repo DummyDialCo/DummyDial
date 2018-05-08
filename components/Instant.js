9c565e9b28faf591361244e026c34b07a0d13aeb
gregsakai <gregsakai@gmail.com>
2018-05-08 12:54:24 -0700

    Spinner added, disappears after 10s

--- components/Instant.js
+++ components/Instant.js
@@ -1,162 +1,194 @@
 import React from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  AsyncStorage,
-  TouchableOpacity,
-  Image
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    AsyncStorage,
+    TouchableOpacity,
+    Image,
+    ActivityIndicator
 } from "react-native";
 import { StackNavigator } from "react-navigation";
 
 import Styles from "./scss/Styles.scss";
 
 export default class Instant extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      recipient: this.props.recipient,
-      myMsg: ""
+        this.state = {
+            recipient: this.props.recipient,
+            myMsg: "",
+            spinnerStyles: Styles.spinnerHidden
+        };
+    }
+
+    componentDidMount() {
+        // Determines if there is already a stored message
+        AsyncStorage.getItem("storeTheMsg")
+            .then(value => {
+                if (value) {
+                    console.log("You have a stored message", value);
+                    this.setState({
+                        myMsg: value
+                    });
+                } else {
+                    this.setState({
+                        myMsg: "Emergency, come now!"
+                    });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    sendCall = () => {
+        fetch("https://dummydial93.herokuapp.com/" + this.state.recipient);
     };
-  }
 
-  componentDidMount() {
-    // Determines if there is already a stored message
-    AsyncStorage.getItem("storeTheMsg")
-      .then(value => {
-        if (value) {
-          console.log("You have a stored message", value);
-          this.setState({
-            myMsg: value
-          });
-        } else {
-          this.setState({
-            myMsg: "Emergency, come now!"
-          });
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+    sendText = () => {
+        fetch(
+            "https://quiet-fortress-33478.herokuapp.com/" +
+                this.state.recipient +
+                "/" +
+                this.state.myMsg
+        );
+    };
 
-  sendCall = () => {
-    fetch("https://dummydial93.herokuapp.com/" + this.state.recipient);
-  };
+    render() {
+        const { navigate } = this.props.navigation;
 
-  sendText = () => {
-    fetch(
-      "https://quiet-fortress-33478.herokuapp.com/" +
-        this.state.recipient +
-        "/" +
-        this.state.myMsg
-    );
-  };
+        return (
+            <View style={Styles.all}>
+                <View style={Styles.tBan}>
+                    <Text>{"\n"}</Text>
+                    <Text style={Styles.tBanTitle}>Instant</Text>
+                </View>
 
-  render() {
-    const { navigate } = this.props.navigation;
+                <Text>
+                    {"\n"}
+                    {"\n"}
+                </Text>
+                <Text> </Text>
+                <Text style={Styles.steps}>
+                    {" "}
+                    Instantly receive a call or text{" "}
+                </Text>
+                <View style={Styles.smBreak2} />
+                <Text style={Styles.blueTxt}>{this.state.recipient}</Text>
 
-    return (
-      <View style={Styles.all}>
-        <View style={Styles.tBan}>
-          <Text>{"\n"}</Text>
-          <Text style={Styles.tBanTitle}>Instant</Text>
-        </View>
+                <Text>{"\n"}</Text>
 
-        <Text>
-          {"\n"}
-          {"\n"}
-        </Text>
-        <Text> </Text>
-        <Text style={Styles.steps}> Instantly receive a call or text </Text>
-        <View style={Styles.smBreak2} />
-        <Text style={Styles.blueTxt}>{this.state.recipient}</Text>
+                <TouchableOpacity style={Styles.btnM} onPress={()=>{
+                    this.setState({ spinnerStyles: Styles.spinner });
+                    this.sendCall();
+                    setTimeout(()=>{
+                        this.setState({ spinnerStyles: Styles.spinnerHidden });
+                    }, 10000);
+                }}>
+                    <View style={Styles.btnMCont}>
+                        <Image
+                            source={require("./imgs/phonew.png")}
+                            style={{ width: 40, height: 43 }}
+                        />
+                        <Text style={Styles.btnMTxt}>Call Now</Text>
+                    </View>
+                </TouchableOpacity>
 
-        <Text>{"\n"}</Text>
+                <View style={Styles.smBreak} />
 
-        <TouchableOpacity style={Styles.btnM} onPress={this.sendCall}>
-          <View style={Styles.btnMCont}>
-            <Image
-              source={require("./imgs/phonew.png")}
-              style={{ width: 40, height: 43 }}
-            />
-            <Text style={Styles.btnMTxt}>Call Now</Text>
-          </View>
-        </TouchableOpacity>
+                <TouchableOpacity style={Styles.btnM} onPress={()=>{
+                    this.setState({ spinnerStyles: Styles.spinner });
+                    this.sendText();
+                    setTimeout(()=>{
+                        this.setState({ spinnerStyles: Styles.spinnerHidden });
+                    }, 10000);
+                }}>
+                    <View style={Styles.btnMCont}>
+                        <Image
+                            source={require("./imgs/textw.png")}
+                            style={{ width: 43, height: 34 }}
+                        />
+                        <Text style={Styles.btnMTxt}>Text Now</Text>
+                    </View>
+                </TouchableOpacity>
 
-        <View style={Styles.smBreak} />
+                <View style={this.state.spinnerStyles}>
+                    <Text style={{ marginBottom: 15, color: "#5CACEE" }}>
+                        Sending...
+                    </Text>
+                    <ActivityIndicator size="large" color="#5CACEE" />
+                </View>
 
-        <TouchableOpacity style={Styles.btnM} onPress={this.sendText}>
-          <View style={Styles.btnMCont}>
-            <Image
-              source={require("./imgs/textw.png")}
-              style={{ width: 43, height: 34 }}
-            />
-            <Text style={Styles.btnMTxt}>Text Now</Text>
-          </View>
-        </TouchableOpacity>
+                <View style={Styles.navBar}>
+                    <TouchableOpacity
+                        onPress={() =>
+                            navigate("Home", {
+                                recipient: this.state.recipient
+                            })
+                        }
+                    >
+                        <View style={Styles.navBarBtn}>
+                            <Image
+                                style={Styles.instantNav}
+                                source={require("./imgs/sphoneb.png")}
+                            />
+                            <Text style={Styles.navTxtB}>Instant</Text>
+                        </View>
+                    </TouchableOpacity>
 
-			<View style={Styles.navBar}>
-          <TouchableOpacity
-            onPress={() =>
-              navigate("Home", { recipient: this.state.recipient })
-            }
-          >
-            <View style={Styles.navBarBtn}>
-              <Image
-			 	style={Styles.instantNav}
-                source={require("./imgs/sphoneb.png")}
-              />
-              <Text style={Styles.navTxtB}>Instant</Text>
+                    <TouchableOpacity
+                        onPress={() =>
+                            navigate("Timer", {
+                                recipient: this.state.recipient
+                            })
+                        }
+                    >
+                        <View style={Styles.navBarBtn}>
+                            <Image
+                                style={Styles.timerNav}
+                                source={require("./imgs/stime.png")}
+                            />
+                            <Text style={Styles.navTxt}>Timer</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() =>
+                            navigate("TextBody", {
+                                recipient: this.state.recipient
+                            })
+                        }
+                    >
+                        <View style={Styles.navBarBtn}>
+                            <Image
+                                style={Styles.textNav}
+                                source={require("./imgs/stext.png")}
+                            />
+                            <Text style={Styles.navTxt}>Text Body</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() =>
+                            navigate("Settings", {
+                                recipient: this.state.recipient
+                            })
+                        }
+                    >
+                        <View style={Styles.navBarBtn}>
+                            <Image
+                                style={Styles.settingsNav}
+                                source={require("./imgs/sgear.png")}
+                            />
+                            <Text style={Styles.navTxt}>Settings</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            onPress={() =>
-              navigate("Timer", { recipient: this.state.recipient })
-            }
-          >
-            <View style={Styles.navBarBtn}>
-              <Image
-				style={Styles.timerNav}
-                source={require("./imgs/stime.png")}
-              />
-              <Text style={Styles.navTxt}>Timer</Text>
-            </View>
-          </TouchableOpacity>
-
-		<TouchableOpacity
-            onPress={() =>
-              navigate("TextBody", { recipient: this.state.recipient })
-            }
-          >
-            <View style={Styles.navBarBtn}>
-              <Image
-				style={Styles.textNav}
-                source={require("./imgs/stext.png")}
-              />
-              <Text style={Styles.navTxt}>Text Body</Text>
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            onPress={() =>
-              navigate("Settings", { recipient: this.state.recipient })
-            }
-          >
-            <View style={Styles.navBarBtn}>
-              <Image
-				style={Styles.settingsNav}
-                source={require("./imgs/sgear.png")}
-              />
-              <Text style={Styles.navTxt}>Settings</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
+        );
+    }
 }
